6661903aa4723c435f6914aa98bc15f372b86123
Staff <epicodus_staff@staff.local>
2019-04-24 14:58:24 -0700

    add advanced leet specs

--- lib/leet.rb
+++ lib/leet.rb
@@ -1,11 +1,42 @@
 class String
   def leet
-    letters = self.split('')
-    letters.map! do |letter|
-      if (letter == 'E') | (letter == 'e')
+    if self.include?(' ')
+      words = self.split()
+      letters = []
+      words.each do |word|
+        split_word = word.split('')
+        split_word.each do |letter|
+          letters.push(letter)
+        end
+        letters.push(' ')
+      end
+      letters.pop
+    else
+      letters = self.split('')
+    end
+    new_letters = letters.each_with_index.map do |letter, i|
+      if (letter == 'S') & (i != 0)
+        if letters[i - 1] == ' '
+          letter
+        else
+          letter  = 'Z'
+        end
+      elsif (letter == 's') & (i != 0)
+        if letters[i - 1] == ' '
+          letter
+        else
+          letter  = 'z'
+        end
+      elsif letter == 'I'
+        letter = 1
+      elsif (letter == 'E') | (letter == 'e')
         letter = 3
+      elsif (letter == 'O') | (letter == 'o')
+        letter = 0
+      else
+        letter
       end
     end
-    letters.join
+    new_letters.join
   end
 end
--- spec/leet_spec.rb
+++ spec/leet_spec.rb
@@ -11,4 +11,20 @@ describe('String#leet') do
     expect('bevy'.leet).to(eq('b3vy'))
   end
 
+  it('converts the letter O into 0') do
+    expect('poop'.leet).to(eq('p00p'))
+  end
+
+  it('converts the capital letter I into 1') do
+    expect('Inky'.leet).to(eq('1nky'))
+  end
+
+  it('converts the letter S into Z unless it is the first letter of the word') do
+    expect('snacks'.leet).to(eq('snackz'))
+  end
+
+  it('can take a multi-word input and correctly leet it') do
+    expect('I see no problems here'.leet).to(eq('1 s33 n0 pr0bl3mz h3r3'))
+  end
+
 end
